dd0a2e3c73f50862524cf7cfb08f0120147629b9
Shido <fdescartes@gmail.com>
2019-08-09 22:41:46 -0300

    create routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,10 @@
 routes = require('express').Router()
+const DevController = require('./controllers/Dev')
+const LikeController = require('./controllers/Like')
+const DisLikeController = require('./controllers/DisLike')
 
-routes.get('/', (req, res) => {
-  return res.json({ message: `Hello World ${req.query.name}` })
-})
-
-routes.post('/devs', (req, res) => {
-  return res.json(req.body)
-})
-
+routes.get('/devs', DevController.index)
+routes.post('/devs', DevController.store)
+routes.post('/devs/:devId/likes', LikeController.store)
+routes.post('/devs/:devId/dislikes', DisLikeController.store)
 module.exports = routes
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
 const routes = require('./routes')
+
 const server = express()
 
+mongoose.connect('mongodb+srv://tindev:tindev@cluster0-ueoot.mongodb.net/tindev?retryWrites=true&w=majority',{
+  useNewUrlParser: true
+})
+
+server.use(cors())
 server.use(express.json())
 server.use(routes)
 server.listen(3000)
